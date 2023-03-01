2c72da837f5af2238588510bdc4e279a06b621fc
nocturnal_hum <thanin.chan@gmail.com>
2023-03-01 13:14:24 -0500

    Rebuild test

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps, title }) {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + '- Weather' : 'Weather'}</title>
+        <meta name='description' content='Weather App' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
